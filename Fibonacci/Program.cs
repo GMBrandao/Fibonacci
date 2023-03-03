ebcb9a2c800ef43cc2de95ba1fba7445242c8c79
GBrandao04 <gabrielmartbrandao@gmail.com>
2023-03-03 17:20:19 -0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿long position, n1 = 0, n2 = 1;
+
+Console.WriteLine("Digite quantos termos a sequência de Fibonacci terá:");
+if (!long.TryParse(Console.ReadLine(), out position))
+{
+    Console.WriteLine("Por favor digite um número válido!");
+}
+else
+{
+    Console.WriteLine("Sequência de Fibonacci:");
+    if (position > 0)
+    {
+        while (position > 0)
+        {
+            Console.Write(n2 + ", ");
+            n2 += n1;
+            n1 = n2 - n1;
+            position--;
+        }
+    }
+    else
+        Console.WriteLine("Número inválido");
+}
